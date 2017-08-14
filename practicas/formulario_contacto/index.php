7c6b9f2161da12b10bd7fe2b36e25faeae09f668
blogdopata <vcaballero_c@hotmail.com>
2017-08-14 18:36:06 -0500

    Formulario ready a produccion

---
+++ practicas/formulario_contacto/index.php
@@ -0,0 +1,53 @@
+<?php 
+// Comprobar si el formulario fue SEND
+//mediante el name del submit
+$errores ="";
+$enviado ="";
+if(isset($_POST['submit'])){
+	$nombre= $_POST['nombre'];
+	$correo= $_POST['correo'];
+	$mensaje= $_POST['mensaje'];
+// si la variable nombre NO esta vacia ejecuta
+	if(!empty($nombre)){
+		$nombre =  trim($nombre);
+		$nombre = filter_var($nombre,FILTER_SANITIZE_STRING);
+	}else{
+		$errores.="Por favor ingresa un nombre<br/>";
+	}
+// si la variable $correo no esta vacia ejecuta
+	if(!empty($correo)){
+		$correo= filter_var($correo,FILTER_SANITIZE_EMAIL);
+		if(!filter_var($correo,FILTER_VALIDATE_EMAIL)){
+			$errores .="Por favor ingrese un Correo Valido <br />";
+		}
+
+	}else{
+		$errores .= "Por favor ingresa un correo <br/>";
+	}
+// si la variable $mensaje no este vacia ejecuta
+	if(!empty($mensaje)){
+		$mensaje = htmlspecialchars($mensaje);
+		$mensaje = trim($mensaje);
+		$mensaje = stripcslashes($mensaje);
+	} else{
+		$errores .= "Por favor Ingreesa el MSJ";
+	}
+// Preguntando si hay errrores
+	if(!$errores){
+		$enviar_a = 'vcaballeroc@usmp.pe';
+		$asunto = 'Correo envaido desde MiSite.com';
+		$mensaje_preparado	= "DE: $nombre \n";
+		$mensaje_preparado .= "Correo: $correo \n";
+		$mensaje_preparado .= "Mensaje: " .$mensaje;
+// Falta subir archivos a un hosting
+		//mail($enviar_a, $asunto, $mensaje_preparado);
+
+	/* con esta linea mostraremos el msj d enviado*/
+		$enviado = 'true';
+	}
+
+}
+
+require 'index_view.php';
+
+ ?>
---
+++ practicas/formulario_contacto/index_view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulario Contacto</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div class="wrap">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+		<input type="text" id="nombre" class="form-control" name="nombre" placeholder="nombre" value="<?php if(!$enviado && isset($nombre)){ echo $nombre; } ?>">
+		
+		<input type="text" id="correo" class="form-control" name="correo" placeholder="correo"  value="<?php if(!$enviado && isset($correo)){ echo $correo; } ?>">
+		
+		<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje: "><?php if(!$enviado && isset($mensaje)){ echo $mensaje; } ?></textarea>
+		<!-- Si la var $erorres NO esta vacia -->
+		<?php  if(!empty($errores)): ?>
+			<div class="alert error">
+				<?php echo $errores; ?>
+			</div>
+		<?php elseif($enviado): ?>
+			<div class="alert success">
+				<p>Enviado Correctamente</p>
+			</div>
+		<?php endif ?>
+
+
+		
+		<input type="submit" name="submit" class="btn btn-primary" value="Enviar correo">
+</form>
+</div>
+</body>
+</html>
