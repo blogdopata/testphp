a6e9058f349f5bb42f599f26cf825793551e376a
blogdopata <vcaballero_c@hotmail.com>
2017-08-25 16:03:46 -0500

    sigle per post

--- practicas/blog/views/header.php
+++ practicas/blog/views/header.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Blog Test</title>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald:400,700" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo RUTA; ?>css/style.css">
+</head>
+<body>
 <header>
 	<div class="contenedor">
 		<div class="logo izquierda">
--- practicas/blog/views/single.view.php
+++ practicas/blog/views/single.view.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Blog Test</title>
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald:400,700" rel="stylesheet">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel="stylesheet" href="<?php echo RUTA; ?>css/style.css">
-</head>
-<body>
 
 <?php require 'views/header.php'; ?>
 
 <div class="contenedor">
 	<div class="post">
 		<article>
-			<h2 class="titulo">Titulo del articulo</h2>
-			<p class="fecha"></p>1 de Enero del  2016
+			<h2 class="titulo"><?php echo $post['titulo']; ?></h2>
+			<p class="fecha"><?php echo fecha($post['fecha']); ?></p>
 			<div class="thumb">
-				<a href="#"><img src="<?php echo RUTA;?>/imagenes/1.png" alt=""></a>
+				<img src="<?php echo RUTA;?>/imagenes/<?php echo $post['thumb']; ?>" alt="">
 			</div>
-			<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ullam dicta sed, asperiores eos quo sint quibusdam ad, impedit qui dolore repellat, error repellendus laborum unde. Enim voluptatum totam maiores? <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quibusdam amet cumque consequuntur eos vel eveniet sapiente fugit nobis sequi quos iusto necessitatibus nesciunt voluptatem laboriosam vitae, rerum quo harum?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit obcaecati assumenda quo ut quod culpa cupiditate atque, ipsa nostrum, consequuntur minima rerum. Praesentium eaque, iste culpa aut optio nobis harum.</p>
+			<p class="extracto"><?php echo $post['texto']; ?></p>
 
 		</article>
 	</div>
