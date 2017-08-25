04b24ff819b445253913368b4f454bc75ce430a4
blogdopata <vcaballero_c@hotmail.com>
2017-08-24 21:58:59 -0500

    config

--- practicas/blog/index.php
+++ practicas/blog/index.php
@@ -1,7 +1,19 @@
 
 <?php 
+
+
+
+
+
 require 'admin/config.php';
 require 'functions.php';
 
+// $conexcion conexion($bd_config);
+
+// if (!$conexion) {
+// 	header('Location:error.php');
+// }
+//obtener_post();
+echo pagina_actual();
 require 'views/index.view.php';
  ?>
